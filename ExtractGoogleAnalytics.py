38c33aa7680d5e4640f71fc6561dc0e677e88e2f
Brian <brian@datno.la>
2015-09-02 17:40:45 -0400

    redoing structure of git

--- ExtractGoogleAnalytics.py
+++ ExtractGoogleAnalytics.py
@@ -7,9 +7,29 @@ import json, datetime, time
 # Important to note - luigi does not like parameters with underscores _ 
 
 class ExtractGoogleAnalytics(luigi.Task):
+    """args:
+    Connection args:
+        viewid = The view id for the google analytics account that you are extracting data from
+        email = The email address for the api credential setup to pull for that google analytics account 
+        p12file = Name of p12 file for api credential setup to pull for that google analytics account 
+                    Save file and store under the p12 directory and rename it to remove spaces
+    Query args:
+        start = Start date of Query
+        end = End date of Query
+        metrics = metrics to include in Query
+        dimensions = dimensions to include in Query
+        filters = how you want to filter the Query
+        sort = how you want to sort the Query
+    returns:
+        CSV file of the google query that is built
+    """
     viewid = luigi.Parameter()
     email = luigi.Parameter()
-    p12file = luigi.Parameter()
+    pfile = luigi.Parameter()
+    metrics = luigi.Parameter()
+    dimensions = luigi.Parameter(default='ga:date')
+    filters = luigi.Parameter(default='ga:date==*')
+    sort = luigi.Parameter(default='ga:date')
 
     one_day_ago = datetime.date.today() - datetime.timedelta(days=1)
     two_days_ago = datetime.date.today() - datetime.timedelta(days=2)
@@ -18,25 +38,34 @@ class ExtractGoogleAnalytics(luigi.Task):
     end = luigi.Parameter(default=one_day_ago)
 
     def run(self):        
-        file_location = 'p12/' + str(self.p12file)
+        file_location = 'p12/' + str(self.pfile)
         scope = ['https://www.googleapis.com/auth/analytics.readonly']
         service = get_ga.get_service('analytics', 'v3', scope, file_location, self.email)
         
-        print(self.start)
-        print('start date string is ' + str(self.start))
-        ga_query = service.data().ga().get(
-          ids='ga:' + self.viewid,
-          start_date=str(self.start), 
-          end_date=str(self.end), 
-          metrics=str(self.metrics),
-          dimensions=str(self.dimensions),
-          sort=str(self.sort),
-          start_index='1').execute()
+        if self.filters == 'ga:date==*':
+            ga_query = service.data().ga().get(
+              ids='ga:' + self.viewid,
+              start_date=str(self.start), 
+              end_date=str(self.end), 
+              metrics=str(self.metrics),
+              dimensions=str(self.dimensions),
+              sort=str(self.sort),
+              start_index='1').execute()
+        else:
+            ga_query = service.data().ga().get(
+              ids='ga:' + self.viewid,
+              start_date=str(self.start), 
+              end_date=str(self.end), 
+              metrics=str(self.metrics),
+              dimensions=str(self.dimensions),
+              filters=str(self.filters),
+              sort=str(self.sort),
+              start_index='1').execute()
+        filename = 'temp_%s_%s.csv' % (self.viewid, unicode(time.strftime('%Y%m%d')))
         
-        writer = csv.writer(open('dict.csv', 'wb'))
         for rows in ga_query:
             if rows == "rows":
-                with open("output.csv", "wb") as f:
+                with open(filename, "wb") as f:
                     writer = csv.writer(f)
                     writer.writerows(ga_query[rows])
         f.close()
